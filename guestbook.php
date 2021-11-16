c7afd6bfb452ca09137c78d6079dc27a094bd6a9
Alexandra Yudintseva <nyaluxey@gmail.com>
2021-11-16 12:26:07 +0700

    Added functionality for registration component

--- guestbook.php
+++ guestbook.php
@@ -19,8 +19,25 @@ if (!$link) {
     die('<p style="color:#9a1f1f">' .mysqli_connect_errno().' - '.mysqli_connect_error().'</p>');
 }
 
-$result = mysqli_query($link, "SELECT * FROM messages;");
+if (isset($_POST["username"]) && isset($_POST["message"])) {
+    $username = $_POST["username"]
+        ? trim(mysqli_real_escape_string($link, $_POST["username"]))
+        : "";
+    $message = $_POST["message"]
+        ? trim(mysqli_real_escape_string($link, $_POST["message"]))
+        : "";
 
+    if (!empty($username) && !empty($message)) {
+        $link->query("INSERT INTO messages(username, message_text) VALUES ('$username', '$message')");
+        if ($link->affected_rows == 1) {
+            echo "<p class='success'>Отправлено</p>";
+        } else {
+            echo "<p class='error'>Ты мышь, получается, раз не отправилось " . $link->error . "</p>";
+        }
+    }
+}
+
+$result = mysqli_query($link, "SELECT * FROM messages;");
 echo '<ul>';
 while ($row = mysqli_fetch_row($result)) {
     echo "<li>{$row[1]}: {$row[2]}, {$row[3]}</li>";
--- registration.php
+++ registration.php
@@ -7,9 +7,34 @@
         <input class="username" name="username" placeholder="Имя пользователя"/>
     </label>
     <label>
-        <input class="password" name="message" type="password" placeholder="Пароль"/>
+        <input class="password" name="password" type="password" placeholder="Пароль"/>
     </label>
     <button class="regBtn" type="submit">Зарегистрироваться</button>
 </form>
 
+<?php
+$link = mysqli_connect('localhost', 'root', '', 'guestbookdb');
+
+if (!$link) {
+    die('<p style="color:#9a1f1f">' .mysqli_connect_errno().' - '.mysqli_connect_error().'</p>');
+}
+
+if (isset($_POST["username"]) && isset($_POST["password"])) {
+    $username = $_POST["username"]
+        ? trim(mysqli_real_escape_string($link, $_POST["username"]))
+        : "";
+    $password = $_POST["password"]
+        ? trim(mysqli_real_escape_string($link, $_POST["password"]))
+        : "";
+
+    if (!empty($username) && !empty($password)) {
+        $link->query("INSERT INTO users(username, password) VALUES ('$username', '$password')");
+        if ($link->affected_rows == 1) {
+            echo "<p class='success'>Регистрация успешна</p>";
+        } else {
+            echo "<p class='error'>Ты мышь, получается, раз не смог зарегистрироваться" . $link->error . "</p>";
+        }
+    }
+}
+?>
 <?php require_once "pages/footer.php"; ?>
